6c0638842be4f48c4a15e4faa962ea6c88eecc3d
Mark Heath <mark.heath@gmail.com>
2022-07-15 11:02:31 +0100

    better handling of failure to talk to catalog service due to mDNS issue

--- frontend/Controllers/EventCatalogController.cs
+++ frontend/Controllers/EventCatalogController.cs
@@ -12,12 +12,17 @@ public class EventCatalogController : Controller
     private readonly IEventCatalogService eventCatalogService;
     private readonly IShoppingBasketService shoppingBasketService;
     private readonly Settings settings;
+    private readonly ILogger<EventCatalogController> logger;
 
-    public EventCatalogController(IEventCatalogService eventCatalogService, IShoppingBasketService shoppingBasketService, Settings settings)
+    public EventCatalogController(IEventCatalogService eventCatalogService, 
+        IShoppingBasketService shoppingBasketService, 
+        Settings settings,
+        ILogger<EventCatalogController> logger)
     {
         this.eventCatalogService = eventCatalogService;
         this.shoppingBasketService = shoppingBasketService;
         this.settings = settings;
+        this.logger = logger;
     }
 
     public async Task<IActionResult> Index()
@@ -26,16 +31,29 @@ public class EventCatalogController : Controller
 
         var getBasket = shoppingBasketService.GetBasket(currentBasketId);
         var getEvents = eventCatalogService.GetAll();
+        var errorMessage = string.Empty;
+        try
+        {
+            await Task.WhenAll(new Task[] { getBasket, getEvents });
+        }
+        catch(Exception ex)
+        {
+            // could be due an mDNS failure to talk to event catalog service when running locally
+            // https://github.com/dapr/dapr/issues/3256
+            logger.LogError(ex, "Failure fetching data for event catalog page");
+            errorMessage = $"Unable to load data: {ex.Message}";
+        }
 
-        await Task.WhenAll(new Task[] { getBasket, getEvents });
-
-        var numberOfItems = getBasket.Result.NumberOfItems;
+        var numberOfItems = getBasket.IsCompletedSuccessfully ? getBasket.Result.NumberOfItems : 0;
+        var events = getEvents.IsCompletedSuccessfully ? getEvents.Result : Array.Empty<Event>();
+        
 
         return View(
             new EventListModel
             {
-                Events = getEvents.Result,
+                Events = events,
                 NumberOfItems = numberOfItems,
+                ErrorMessage = errorMessage
             }
         );
     }
--- frontend/Models/View/EventListModel.cs
+++ frontend/Models/View/EventListModel.cs
@@ -4,6 +4,7 @@ namespace GloboTicket.Frontend.Models.View;
 
 public class EventListModel
 {
+    public string ErrorMessage { get; set; } = string.Empty;
     public IEnumerable<Event> Events { get; set; } = new List<Event>();
     public int NumberOfItems { get; set; }
 }
